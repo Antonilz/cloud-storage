305be7851bffa1e3991a24bf8e6842e2edbd47cc
antonilz <antonilz.web@gmail.com>
2018-09-18 20:01:26 +0300

    proper column count when resizing viewport

--- client/src/pages/StorageBrowser/components/FilesViewGrid.js
+++ client/src/pages/StorageBrowser/components/FilesViewGrid.js
@@ -12,7 +12,6 @@ import {
 } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { Grid as VGrid, AutoSizer, WindowScroller } from 'react-virtualized';
-//import 'react-virtualized/styles.css';
 import GridCell from '../containers/GridCell';
 
 const StyledGrid = styled(VGrid)`
@@ -32,69 +31,74 @@ const StyledGrid = styled(VGrid)`
 `;
 
 class FilesViewGrid extends PureComponent {
+  state = {
+    columnCount: 5
+  };
+
   cellRenderer = ({ key, columnIndex, rowIndex, style }) => {
-    const index = rowIndex * 4 + columnIndex;
-    return index < this.props.itemsIDs.length ? (
-      <GridCell
-        id={this.props.itemsIDs[index]}
-        //index={index}
-        key={key}
-        itemsCount={this.props.itemsIDs.length}
-        style={{ ...style, overflow: 'visible' }}
-        onFolderClick={this.props.onFolderClick}
-        onFileClick={this.props.onFileClick}
-        onItemCheck={this.props.onItemCheck}
-        onItemRenameToggle={this.props.onItemRenameToggle}
-        onItemsTagsEditToggle={this.props.onItemsTagsEditToggle}
-        deleteSelectedItems={this.props.deleteSelectedItems}
-      />
-    ) : null;
+    const index = rowIndex * this.state.columnCount + columnIndex;
+    return (
+      index < this.props.itemsCount && (
+        <GridCell
+          id={this.props.itemsIDs[index]}
+          key={key}
+          itemsCount={this.props.itemsIDs.length}
+          style={{ ...style, overflow: 'visible' }}
+          onFolderClick={this.props.onFolderClick}
+          onFileClick={this.props.onFileClick}
+          onItemCheck={this.props.onItemCheck}
+          onItemRenameToggle={this.props.onItemRenameToggle}
+          onItemsTagsEditToggle={this.props.onItemsTagsEditToggle}
+          deleteSelectedItems={this.props.deleteSelectedItems}
+        />
+      )
+    );
+  };
+
+  calculateColumnCount = ({ width, height }) => {
+    console.log(width);
+    if (width <= 768) {
+      this.setState({
+        columnCount: 3
+      });
+    } else {
+      this.setState({
+        columnCount: 5
+      });
+    }
   };
 
   render() {
     const { selectedItemsCount, itemsIDs } = this.props;
+    const { columnCount } = this.state;
+    console.log(this.props);
     return (
-      <div>
-        {itemsIDs.length > 0 && (
-          <WindowScroller>
-            {({ height, isScrolling, onChildScroll, scrollTop }) => (
-              <AutoSizer disableHeight>
-                {({ width }) => {
-                  let itemsCount = 5;
-                  if (width <= 768) {
-                    itemsCount = 3;
-                  } else if (width > 768 && width <= 1280) {
-                    itemsCount = 5;
-                  } else {
-                    itemsCount = 5;
-                  }
-
-                  return (
-                    <StyledGrid
-                      autoHeight
-                      scrollTop={scrollTop}
-                      isScrolling={isScrolling}
-                      onScroll={onChildScroll}
-                      width={width}
-                      height={height}
-                      //rowHeight={180}
-                      rowHeight={width / itemsCount}
-                      columnWidth={width / itemsCount}
-                      columnCount={itemsCount}
-                      rowCount={Math.ceil(
-                        this.props.itemsIDs.length / itemsCount
-                      )}
-                      overScanRowCount={10}
-                      items={this.props.itemsIDs}
-                      cellRenderer={this.cellRenderer}
-                    />
-                  );
-                }}
-              </AutoSizer>
-            )}
-          </WindowScroller>
+      <WindowScroller>
+        {({ height, isScrolling, onChildScroll, scrollTop }) => (
+          <AutoSizer disableHeight onResize={this.calculateColumnCount}>
+            {({ width }) => {
+              return (
+                <StyledGrid
+                  autoHeight
+                  scrollTop={scrollTop}
+                  isScrolling={isScrolling}
+                  onScroll={onChildScroll}
+                  width={width}
+                  height={height}
+                  //rowHeight={180}
+                  rowHeight={width / columnCount}
+                  columnWidth={width / columnCount}
+                  columnCount={columnCount}
+                  rowCount={Math.ceil(itemsIDs.length / columnCount)}
+                  overScanRowCount={10}
+                  items={itemsIDs}
+                  cellRenderer={this.cellRenderer}
+                />
+              );
+            }}
+          </AutoSizer>
         )}
-      </div>
+      </WindowScroller>
     );
   }
 }
